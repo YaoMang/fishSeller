a055c44967e61decf00a6b51f00c2bcf0bb0430d
YaoMang <YaoMangYM@gmail.com>
2023-04-29 19:37:11 +0800

    framework

---
+++ opensoda.py
@@ -0,0 +1,16 @@
+import json
+
+def input (data:str):
+    ret:dict = json.loads(data)
+
+    return ret
+
+def calStat (data:dict):
+    ret:dict
+    
+    return ret
+
+def output (data:str):
+    ret = json.dumps(data)
+
+    return ret
