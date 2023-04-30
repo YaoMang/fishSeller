4fa859851166e22a147deca6cabeef4ee181c817
YaoMang <YaoMangYM@gmail.com>
2023-04-30 21:10:09 +0800

    debuging...

--- opensoda.py
+++ opensoda.py
@@ -1,10 +1,8 @@
 import json
 import requests
 
-# fetch data using GitHub REST API
-
-# get history star num
-
+# fetch data with GitHub REST API
+# support not only a single API
 class getInfo():
     host:str
     owner:str
@@ -20,8 +18,11 @@ class getInfo():
     def __str__(self) -> str:
         return self.host + self.owner + self.repo
 
-    def get_pages(self, rsp) -> None:
-         return
+    def get_pages(self, links:dict) -> None:
+        next_dict = links["next"]
+        last_dict = links["last"]
+        last_dict["url"]
+        return
 
     def get_repo_stargazers(self) -> dict:
         # get the api addr
@@ -29,14 +30,14 @@ class getInfo():
         
         # get stargazers list with star time
         rsp = requests.get(url, headers={"Accept":"application/vnd.github.v3.star+json"})
-        text = rsp.text
-
+        
+        # get page data of stargazers
+        self.get_pages(rsp.links)
+        
         # parse .json and convert it to dict
-        data:dict = json.loads(text)
+        data:dict = json.loads(rsp.text)
 
         return data
-    
-
     
 def combineUrl(*args) -> str:
         url = "https:/"
@@ -58,4 +59,4 @@ def output (data:str):
 
 info = getInfo("api.github.com", "github-tools", "github")
 data = info.get_repo_stargazers()
-print(data)
+#print(data)
