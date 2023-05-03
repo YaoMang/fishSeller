4d5f38f6e95afad5d69a70c5fe29718fb926bf78
NEU-YuanShen <1016729788@qq.com>
2023-05-03 19:01:29 +0800

    read all file

--- read_data.py
+++ read_data.py
@@ -1,6 +1,8 @@
 import os
 import json
 import PAM
+import matplotlib.pyplot as plt
+import re
 
 # def list_dir(file_dir):
 #   '''
@@ -18,15 +20,29 @@ import PAM
 #       print ("{0} : is dir!".format(cur_file))
 #       list_dir(path) # 递归子目录
 
+# string = "/top_300_metrics/CleverRaven/Cataclysm-DDA"
+# regex = r"/([^/]+)$"
+# match = re.search(regex, string)
+
+# if match:
+#     result = match.group(1)
+#     print(result)  # 输出 "Cataclysm-DDA"
+# else:
+#     print("没有匹配到结果。")
+
 
 def read_json(project_path, json_file_list: str):
+    counter = 1
     seven_metricsDict_list = []
     for project in project_path:
         for json_file in json_file_list:
-            jsonfile = open(project+"/"+json_file)
+            try:
+                jsonfile = open(project+"/"+json_file)
+            except FileNotFoundError:
+                print("无法打开文件 " + project+"/"+json_file + "。请检查文件名是否正确。")
+                break
             json_dict: dict = json.loads(jsonfile.read())
             seven_metricsDict_list.append(json_dict)
-        print(seven_metricsDict_list)
         pam = PAM.PAM(seven_metricsDict_list[0],
                       seven_metricsDict_list[1],
                       seven_metricsDict_list[2],
@@ -34,8 +50,20 @@ def read_json(project_path, json_file_list: str):
                       seven_metricsDict_list[4],
                       seven_metricsDict_list[5],
                       seven_metricsDict_list[6])
-        print(pam.getPAM(["2017-01", "2020-01"]))
-        break
+        evolutionScore: dict = pam.getPAM(["2017-01", "2020-01"])
+        txt = json.dumps(evolutionScore)
+        regex = r"/([^/]+)$"
+        match = re.search(regex, project)
+        match = match.group(0)
+        print(str(counter)+"   ./output"+match+".json")
+        counter += 1
+        f = open("./output/"+match+".json", "w+")
+        f.write(txt)
+        # x = list(evolutionScore.keys())
+        # y = list(evolutionScore.values())
+        # print(x)
+        # print(y)
+        # plt.plot(x, y)
 
     # json_dict:dict=json.loads(jsonfile.read())
     # print(json_dict)
